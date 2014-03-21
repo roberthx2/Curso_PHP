ba61e4b74026567dec45dcde03d2aba9b97c50a1
angeldanielboscan <angeldanielboscan@hotmail.com>
2014-03-21 17:01:58 -0430

    Actualizacion
    inclusion de archivos nuevos, como el include para la demostracion de
    como funciona, el footer y header, con el mismo proposito y por ultimo
    el formulario para enseñar como se procesan formularios en php, falta el
    procesar.php

--- include1.php
+++ include1.php
@@ -1,7 +1,3 @@
-<!DOCTYPE html>
-<html lang = "es">
-<meta charset = "utf-8"/>
-<body>
 <?php
 
 echo "Desde Aquí...";
@@ -10,6 +6,3 @@ echo "<h1>Saludos, este es el Curso de PHP Básico</h1>
 echo "Hasta aquí... Es el documento que incluímos";
 
 ?>
-
-</body>
-</html>
