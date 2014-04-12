48b3d232184ed0c132ff256d4fb6f579fcac7484
angeldanielboscan <angeldanielboscan@hotmail.com>
2014-04-11 22:15:06 -0430

    Correccion
    Se corrgie los codigo de bases de datos con PHP

--- eliminar.php
+++ eliminar.php
@@ -4,13 +4,14 @@ include 'db-conf.php';
 
 $con = mysql_connect($host,$user,$pwd)
 or die("Fallo en la conexion con la base de datos");
+mysql_select_db($db, $con) or die("Fallo al conectar con $db");
 
-$reg = mysql_query("SELECT id FROM Curso WHERE nombre = '$_POST[nombre]'", $con)
+$reg = mysql_query("SELECT id FROM persona WHERE nombre = '$_POST[nombre]'", $con)
 or die("Fallo en el query");
 
 if ($re = mysql_fetch_array($reg)){
 
-mysql_query("DELETE FROM Curso WHERE NOMBRE = '$_POST[nombre]'", $con);
+mysql_query("DELETE FROM persona WHERE NOMBRE = '$_POST[nombre]'", $con);
 
 echo "registros eliminados";
 
--- seleccion.php
+++ seleccion.php
@@ -4,14 +4,16 @@ include 'db-conf.php';
 
 $con = mysql_connect($host, $user, $pwd)
 or die ("problemas al conectar con el servidor de Bases de datos");
+mysql_select_db($db, $con) or die("Fallo al conectar con $db");
 
-$registro = mysql_query("SELECT * FROM Curso WHERE nombre = '$_POST[nombre]'",$con)
-or die ("Problemas con el query", mysql_error());
+$registro = mysql_query("SELECT * FROM persona WHERE nombre = '$_POST[nombre]'", $con)
+or die ("Problemas con el query");
 
 while ($reg = mysql_fetch_array($registro)){
 
-echo $reg['NOMBRE']."<br>";
-echo $reg['CEDULA']."<br>";
+echo $reg['nombre']."<br>";
+echo $reg['cedula']."<br>";
 
 }
+
 ?>
