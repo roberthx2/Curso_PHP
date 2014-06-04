d31a966fb51c27e9e11de28bf31b15c6b1df3fd7
roberth riera <riera_1992@hotmail.com>
2014-06-04 13:20:03 -0430

    Create imprimirWhile.php
    pide un numero

---
+++ imprimirWhile.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang = "es">
+<meta charset = "utf-8"/>
+<head>
+    <title> Ciclos while</title>
+</head>
+<body>
+	<form method = "GET" action = "procesarImprimirWhile.php">
+			¿Cuantas veces?
+			<input type = "text" name = "num" autofocus></input> 
+			<input type = "submit" value = "enviar">
+		</form><br>
+
+</body>
+</html>
