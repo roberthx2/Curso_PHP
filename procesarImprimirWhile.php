0415fdc9ccce7173db08f2bfaffdd8a1a1049217
roberth riera <riera_1992@hotmail.com>
2014-06-04 13:21:24 -0430

    Create procesarImprimirWhile.php
    imprime el la cantidad de veces que se dijo antes

---
+++ procesarImprimirWhile.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang = "es">
+<meta charset = "utf-8"/>
+<head>
+    <title> Ciclos while2</title>
+</head>
+<body>
+	<?php
+		$i = 1;
+		
+		while($i <= $_GET['num'])
+		{
+			if ($i%2)
+				echo "$i <br>";
+			$i++;
+		}
+	?>
+</body>
+</html>
